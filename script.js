2b2e1040e89bf2acd795bfcb1730e17a9feb845c
MatthewLoiselle <>
2021-04-29 15:28:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('imagebutton').addEventListener('click', imageswap)
+
+function imageswap () {
+  document.getElementById('shrimp').style.display = "none"
+  document.getElementById('deadshrimp').style.display = "block"
+}
