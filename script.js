fe5df650111ab9d16dccf8830cdd19c2d6eeab88
MatthewLoiselle <>
2021-04-29 16:10:37 +0000

    style fixes

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 document.getElementById('imagebutton').addEventListener('click', imageswap)
-
-function imageswap () {
-  document.getElementById('shrimp').style.display = "none"
-  document.getElementById('deadshrimp').style.display = "block"
-}
-
+document.getElementById('backgroundbutton').addEventListener('click', background)
+document.getElementById('textswapper').addEventListener('click', swaptext)
 document.getElementById('textbutton').addEventListener('click', hidetext)
 
-function hidetext () {
-  document.getElementById('SecondText').style.display = "none"
+function imageswap () {
+  document.getElementById('shrimp').style.display = 'none'
+  document.getElementById('deadshrimp').style.display = 'block'
 }
 
-document.getElementById('textswapper').addEventListener('click', swaptext)
+function hidetext () {
+  document.getElementById('SecondText').style.display = 'none'
+}
 
 function swaptext () {
-  document.getElementById('FirstText').innerHTML = "and it did"
+  document.getElementById('FirstText').innerHTML = 'and it did'
 }
 
-document.getElementById('backgroundbutton').addEventListener('click', background)
-
 function background () {
-  document.body.style.backgroundColor = "red"
+  document.body.style.backgroundColor = 'red'
 }
